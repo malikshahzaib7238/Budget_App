0502f785424cf3b4e19c80d636f6d7e0829f0e0f
Malik Shahzaib <malikshahzaib7238@gmail.com>
2024-01-21 15:50:31 +0500

    submitted

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,15 +1,30 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
 const Remaining = () => {
-    const { expenses, budget } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total = total + item.cost);
-    }, 0);
-    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
-    return (
-        <div className={`alert ${alertType}`}>
-            <span>Remaining: £{budget - totalExpenses}</span>
-        </div>
-    );
+  const { expenses, budget, remaining, currency } = useContext(AppContext);
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total = total + item.cost);
+  }, 0);
+
+  const isBudgetExceeded = totalExpenses > budget;
+  const isRemainingNegative = remaining < 0;
+
+  if (isRemainingNegative) {
+    // Use window.alert to display a prompt when budget is lower than remaining
+    window.alert("You cannot reduce the budget value lower than the spending");
+  }
+
+  const alertType = isBudgetExceeded ? "alert-danger" : "alert-success";
+
+  return (
+    <div className={`alert ${alertType}`}>
+      <span>
+        Remaining: {currency}
+        {budget - totalExpenses}
+      </span>
+    </div>
+  );
 };
+
 export default Remaining;
